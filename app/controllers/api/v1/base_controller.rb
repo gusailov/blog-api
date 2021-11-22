818d758988e1841e681b183909e81892ee4bd95e
gusailov <gusailov@gmail.com>
2021-11-22 21:16:59 +0200

    add record_not_found to base_controller.rb

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::BaseController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found(error)
+    render json: { error: error.to_s }, status: :not_found
+  end
 end
