45943a1643ba488f6d90b8cf7beac75ef3b1f1a4
Gusailov <gusailov@gmail.com>
2023-03-02 14:11:40 +0200

    add admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.admin.create(provider: "email", email:"admin@mail.com", password: ENV.fetch('ADMIN_PASSWORD'))
