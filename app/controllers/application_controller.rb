e80bfac69e1ef938824e098d24ddfcc28361c91b
Gusailov <gusailov@gmail.com>
2023-03-02 12:47:46 +0200

    remove ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  include Pundit
+  include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError do
     head :forbidden
